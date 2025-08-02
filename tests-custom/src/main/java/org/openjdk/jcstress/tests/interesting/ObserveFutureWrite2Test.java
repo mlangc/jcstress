d1b7c882e5cf25600db37fbfc8e00ae39465b7a7
Matthias Langer <m.langer798@gmail.com>
2025-08-02 19:39:39 +0000

    Working on tests for blog post

---
+++ tests-custom/src/main/java/org/openjdk/jcstress/tests/interesting/ObserveFutureWrite2Test.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2014, 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jcstress.tests.interesting;
+
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.I_Result;
+
+@JCStressTest
+@Description("Attempts to observe a write from the future")
+@Outcome(id = "0", expect = Expect.ACCEPTABLE)
+@Outcome(id = "42", expect = Expect.ACCEPTABLE_INTERESTING, desc = "Observed future write")
+@State
+public class ObserveFutureWrite2Test {
+    static final int LEN = 64;
+
+    final int[] ints = new int[LEN];
+
+    @Actor
+    public void actor1() {
+        ints[0] = ints[LEN - 1];
+    }
+
+    @Actor
+    public void actor2(I_Result r) {
+        r.r1 = ints[0];
+        ints[LEN - 1] = 42;
+    }
+}
