cb807b009d51b09860cabe099e201bcd1a47996f
Matthias Langer <m.langer798@gmail.com>
2025-08-02 15:57:50 +0200

    Add custom test for blog post

---
+++ tests-custom/src/main/java/org/openjdk/jcstress/tests/atomics/integer/AtomicIntegerSetGetOpaqueTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2014, 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
+package org.openjdk.jcstress.tests.atomics.integer;
+
+import org.openjdk.jcstress.annotations.*;
+import org.openjdk.jcstress.infra.results.III_Result;
+
+import java.util.concurrent.atomic.AtomicInteger;
+
+@JCStressTest
+@Description("Demonstrates the behaviour of opaque mode")
+@Outcome(id = "0, 0, 0", expect = Expect.ACCEPTABLE)
+@Outcome(id = "0, 0, 1", expect = Expect.ACCEPTABLE)
+@Outcome(id = "0, 0, 2", expect = Expect.ACCEPTABLE)
+@Outcome(id = "0, 1, 0", expect = Expect.ACCEPTABLE)
+@Outcome(id = "0, 1, 1", expect = Expect.ACCEPTABLE)
+@Outcome(id = "0, 1, 2", expect = Expect.ACCEPTABLE)
+@Outcome(id = "1, 0, 0", expect = Expect.FORBIDDEN, desc = "b=1 --> b=0")
+@Outcome(id = "1, 0, 1", expect = Expect.ACCEPTABLE_INTERESTING, desc = "b=1 but a=0")
+@Outcome(id = "1, 0, 2", expect = Expect.ACCEPTABLE_INTERESTING, desc = "b=1 but a=0")
+@Outcome(id = "1, 1, 0", expect = Expect.FORBIDDEN, desc = "b=1 --> b=0")
+@Outcome(id = "1, 1, 1", expect = Expect.ACCEPTABLE)
+@Outcome(id = "1, 1, 2", expect = Expect.ACCEPTABLE)
+@Outcome(id = "2, 0, 0", expect = Expect.FORBIDDEN, desc = "b=2 --> b=0")
+@Outcome(id = "2, 0, 1", expect = Expect.FORBIDDEN, desc = "b=2 --> b=1")
+@Outcome(id = "2, 1, 0", expect = Expect.FORBIDDEN, desc = "b=2 --> b=0")
+@Outcome(id = "2, 1, 1", expect = Expect.FORBIDDEN, desc = "b=2 --> b=1")
+@Outcome(id = "2, 1, 2", expect = Expect.ACCEPTABLE)
+@State
+public class AtomicIntegerSetGetOpaqueTest {
+    final AtomicInteger a = new AtomicInteger();
+    final AtomicInteger b = new AtomicInteger();
+
+    @Actor
+    public void actor1() {
+        a.setOpaque(1);
+        b.setOpaque(1);
+        b.setOpaque(2);
+    }
+
+    @Actor
+    public void actor2(III_Result r) {
+        r.r1 = b.getOpaque();
+        r.r2 = a.getOpaque();
+        r.r3 = b.getOpaque();
+    }
+}
