89bad4520923ab05cde9d564e4a3ad74f287b7b4
Matthias Langer <m.langer798@gmail.com>
2025-08-03 18:44:58 +0000

    Working on tests for blog post

--- tests-custom/src/main/java/org/openjdk/jcstress/tests/atomics/references/AtomicReferenceSetGetOpaqueTest.java
+++ tests-custom/src/main/java/org/openjdk/jcstress/tests/atomics/references/AtomicReferenceSetGetOpaqueTest.java
@@ -1,20 +1,42 @@
+/*
+ * Copyright (c) 2014, 2025, Oracle and/or its affiliates. All rights reserved.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This code is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 2 only, as
+ * published by the Free Software Foundation.  Oracle designates this
+ * particular file as subject to the "Classpath" exception as provided
+ * by Oracle in the LICENSE file that accompanied this code.
+ *
+ * This code is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
+ * version 2 for more details (a copy is included in the LICENSE file that
+ * accompanied this code).
+ *
+ * You should have received a copy of the GNU General Public License version
+ * 2 along with this work; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ * Please contact Oracle, 500 Oracle Parkway, Redwood Shores, CA 94065 USA
+ * or visit www.oracle.com if you need additional information or have any
+ * questions.
+ */
 package org.openjdk.jcstress.tests.atomics.references;
 
 import org.openjdk.jcstress.annotations.*;
 import org.openjdk.jcstress.infra.results.I_Result;
 
-import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicReference;
 
 @JCStressTest
 @Description("Shows why opaque mode must not used to publish objects with non final fields")
-@Outcome(id = "2", expect = Expect.ACCEPTABLE)
-@Outcome(id = "1", expect = Expect.ACCEPTABLE)
+@Outcome(id = "42", expect = Expect.ACCEPTABLE)
 @Outcome(id = "0", expect = Expect.ACCEPTABLE_INTERESTING, desc = "observed partially constructed object")
 @State
 public class AtomicReferenceSetGetOpaqueTest {
     static class Holder {
-        int x = ThreadLocalRandom.current().nextInt(1, 3);
+        int x = 42;
     }
 
     final AtomicReference<Holder> ref = new AtomicReference<>();
